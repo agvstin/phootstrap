67503c3b0524201ca993a2da719ee0dceaec7147
Agustín Gutiérrez <agu.gutierrez@gmail.com>
2013-03-02 09:19:24 -0300

    Fixed typo. Added symfony/translation and monolog dependencies. Added service providers: Url, Session, Translation and Monolog

--- app/autoload.php
+++ app/autoload.php
@@ -1,6 +1,6 @@
 <?php
 
-$autoloader = require_once __DIR__ .'/../vendors/autoload.php';
+$autoloader = require_once __DIR__ .'/../vendor/autoload.php';
 
 // define your app's namespace...
 // $autoloader->add('MyApp', __DIR__ .'/../src');
--- app/bootstrap.php
+++ app/bootstrap.php
@@ -2,7 +2,7 @@
 require_once __DIR__ .'/autoload.php';
 
 use Silex\Application as App;
-use Silex\Provider\TwigServiceProvider;
+use Silex\Provider;
 
 defined('ROOT_PATH') or define('ROOT_PATH', __DIR__ .'/..');
 
@@ -40,8 +40,29 @@ $app['db'] = $app->share(function($app) {
   return new Redbean_Facade;
 });
 
-$app->register(new TwigServiceProvider, array(
+// register providers
+$app->register(new Provider\SessionServiceProvider());
+$app->register(new Provider\UrlGeneratorServiceProvider());
+$app->register(new Provider\TwigServiceProvider, array(
   'twig.path' => __DIR__ .'/../web/views'
+));
+$app->register(new Provider\TranslationServiceProvider(), array(
+  'locale_fallback' => 'en',
+));
+$app['translator.domains'] = $app->share(function($app) {
+  $locales = glob(__DIR__ .'/../data/locales/*.ini');
+  $domains = array();
+  foreach ($locales as $locale) {
+    list($lang, ) = explode('.', basename($locale));
+    $domains['messages'][$lang] = parse_ini_file($locale);
+  }
+
+  return $domains;
+});
+
+$app->register(new Provider\MonologServiceProvider(), array(
+    'monolog.logfile' => $app['config.value']('log.file', __DIR__ .'/../data/logs/' .$app['env'] .'.log'),
+    'monolog.level' => $app['config.value']('log.level', 'info'),
 ));
 
 return $app;
